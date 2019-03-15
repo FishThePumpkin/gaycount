f2671621266309844094748b8c0357c065c5da5a
FishThePumpkin <39113773+FishThePumpkin@users.noreply.github.com>
2019-03-15 23:56:55 +1100

    Update jessie.py

--- jessie.py
+++ jessie.py
@@ -35,7 +35,6 @@ async def on_message(message):
             print('gay')
             await client.send_message(message.channel, 'Jessie said gay')
     if message.content == 'jessie is gay':
-            global jessie
             await client.say("Jessie has said gay %d times" % jessie) 
 
 @client.command()
