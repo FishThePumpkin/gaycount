46e63d2b9b78a051e79d3c33623572a78ea16038
FishThePumpkin <39113773+FishThePumpkin@users.noreply.github.com>
2019-03-17 00:14:59 +1100

    Update jessie.py

--- jessie.py
+++ jessie.py
@@ -9,7 +9,7 @@ import os
 client = commands.Bot(command_prefix = "-")
 client.remove_command('help')
 status = ['Cat\'s', 'voice', 'is', 'so', 'nice', 'owo']
-jessie = 100
+jessie = 99
 
 async def change_status():
     await client.wait_until_ready()
