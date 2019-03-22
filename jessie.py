b5f239309aeccf9aef3bf3c975c48cecb241d154
FishThePumpkin <39113773+FishThePumpkin@users.noreply.github.com>
2019-03-23 00:11:22 +1100

    Update jessie.py

--- jessie.py
+++ jessie.py
@@ -21,7 +21,7 @@ IDs = {
 }
 permissions = [IDs["Vivian"],IDs["Owner"]]
 imnotPERMS = [IDs["Rachel"],IDs["Labib"]]
-jessie = 148
+jessie = 155
 
 async def change_status():
     await client.wait_until_ready()
